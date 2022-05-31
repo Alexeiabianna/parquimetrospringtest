d104937131f9555f61a5fa0588a2a1b597376e0b
Alexei Militz Abianna <20101204@facin.portoalegre.pucrsnet.br>
2022-05-31 18:31:00 -0300

    initial commit

---
+++ src/main/java/com/alexei/pucrs/App.java
@@ -0,0 +1,13 @@
+package com.alexei.pucrs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
